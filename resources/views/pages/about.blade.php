74770f835756de7d575eecec6e1249507018bbfb
Link1986 <ali.ghoudi@gmail.com>
2021-06-21 21:35:07 +0200

    Add styling with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
-        <img src="{{ asset('images/link1986.png') }}" alt="link1986-avatar">
+        <img src="{{ asset('images/link1986.png') }}" alt="link1986-avatar" class="my-12 rounded-full shadow-md">
 
-        <p>Built with &hearts; by Link1986.</p>
+        <h2 class="mb-5 text-gray-700">Built with <span class="text-pink-500">&hearts;</span> by Link1986.</h2>
 
-        <p><a href="/">Revenir à la page d'accueil</a></p>
+        <p><a href="/" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'accueil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,9 @@
 @section('title')
 
 @section('content')
-        <img src="{{ asset('images/France.png') }}" alt="France-flag" />
+        <img src="{{ asset('images/France.png') }}" alt="France-flag" class="mt-12 rounded shadow-md h-32" />
 
-        <h1>Hello from France!</h1>
+        <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from France!</h1>
 
-        <p>It's currently {{date('H:i:s')}}.</p>
+        <p class="text-lg text-gray-800">It's currently {{date('H:i:s')}}.</p>
 @endsection
